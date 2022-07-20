54875c369b98d633553e54c426a99b4cb595fede
Shashi Kumar <66266334+shashisk97@users.noreply.github.com>
2022-07-20 16:13:18 +0100

    Creating state.ts

---
+++ state.ts
@@ -0,0 +1,20 @@
+terraform {
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.49.0"
+    }
+
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "1.6.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  alias = "cftappsdemo"
+  features {}
+  subscription_id = "d025fece-ce99-4df2-b7a9-b649d3ff2060"
+}
