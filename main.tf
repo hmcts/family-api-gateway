cf37479cff5dcd0e6dfb79ba67a95218d517f866
Shashi Kumar <66266334+shashisk97@users.noreply.github.com>
2022-07-21 14:33:53 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ data "azurerm_key_vault_secret" "s2s_client_secret" {
   name         = "gateway-s2s-client-secret"
   key_vault_id = data.azurerm_key_vault.fis_key_vault.id
 }
-
+  
 resource "azurerm_api_management_user" "courtnav_user" {
   api_management_name = local.api_mgmt_name
   resource_group_name = local.api_mgmt_rg
