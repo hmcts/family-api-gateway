194a5ca71c716744304c849ff721a39130b250c2
Shashi Kumar <66266334+shashisk97@users.noreply.github.com>
2022-07-22 10:51:54 +0100

    copying suscription key to vault

--- court_nav.tf
+++ court_nav.tf
@@ -72,3 +72,9 @@ resource "azurerm_api_management_subscription" "courtnav_subscription" {
   provider = azurerm.cftappsdemo
 }
 
+resource "azurerm_key_vault_secret" "subscription_key" {
+  name         = "courtnav-subscription-sub-key"
+  value        = azurerm_api_management_subscription.courtnav_subscription.primary_key
+  key_vault_id = data.azurerm_key_vault.fis_key_vault.id
+  tags         = local.common_tags
+}
