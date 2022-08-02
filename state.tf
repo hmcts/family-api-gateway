a4dbbc74f10aa8fdad1caadd092bf59e29651e44
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2022-08-02 16:47:31 +0100

    Refactor

--- state.tf
+++ state.tf
@@ -6,8 +6,9 @@ terraform {
   backend "azurerm" {}
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.49.0"
+      source                = "hashicorp/azurerm"
+      version               = "3.10.0"
+      configuration_aliases = [azurerm.aks-cftapps]
     }
 
     azuread = {
