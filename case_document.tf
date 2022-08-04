c0d2f6a8ddd362fb1c85f2913be2bd0216e8aba0
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2022-08-04 16:50:07 +0100

    Changes API swagger file name

--- case_document.tf
+++ case_document.tf
@@ -22,7 +22,7 @@ module "document-mgmt-api" {
   name          = join("-", [var.document_product_name, "api"])
   display_name  = "Case document api"
   path          = "prl-document-api"
-  swagger_url   = "https://raw.githubusercontent.com/hmcts/reform-api-docs/master/docs/specs/document_upload.json"
+  swagger_url   = "https://raw.githubusercontent.com/hmcts/reform-api-docs/master/docs/specs/prl_document_upload.json"
 
   providers     = {
     azurerm = azurerm.aks-cftapps
