eb5ec215fe9ad47c2a14f34eb9a60436022d1e37
Yogendra Upasani <yogendra.upasani@hmcts.net>
2023-08-10 18:55:19 +0100

    upgrade azurerm version

--- case_document.tf
+++ case_document.tf
@@ -22,7 +22,6 @@ module "document-mgmt-api" {
   path          = "prl-document-api"
   protocols     = ["http", "https"]
   swagger_url   = "https://raw.githubusercontent.com/hmcts/reform-api-docs/master/docs/specs/prl_document_upload.json"
-  api_type      = "http"
 }
 
 data "template_file" "document_policy_template" {
