5dbd60cd9a90575ad091d2314e17b0bf3711eac5
yogendraU <66823759+yogendraU@users.noreply.github.com>
2023-08-11 15:58:14 +0100

    Update case_get_document.tf
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- case_get_document.tf
+++ case_get_document.tf
@@ -18,7 +18,7 @@ module "case-document-mgmt-api" {
   revision      = "1"
   service_url   = local.prl_api_url
   product_id    = module.case-document-mgmt-product.product_id
-  name          = join("-", [var.document_product_get_name, "api"])
+  name          = "${var.document_product_get_name}-api"
   display_name  = "get case document for family api"
   path          = "family-document-api"
   protocols     = ["http", "https"]
