574cb449e52b2cdd1a7796ac0a0e28a5f90a1a79
bharadwajsairam <bharadwajsairam001@gmail.com>
2024-11-11 17:43:55 +0000

    Update & test resource address

--- import.tf
+++ import.tf
@@ -5,7 +5,16 @@ import {
   # Run this import only in prod
   for_each = var.env == "demo" ? toset(["import"]) : toset([])
   # Specify the resource address to import to
-  to = azurerm_api_management_subscription.resource_group_name
+  to = module.document-mgmt-product.azurerm_api_management_product.product
   # Specify the resource ID to import from
   id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/products/document"
+}
+
+import {
+  # Run this import only in prod
+  for_each = var.env == "demo" ? toset(["import"]) : toset([])
+  # Specify the resource address to import to
+  to = module.document-mgmt-api.azurerm_api_management_api.api
+  # Specify the resource ID to import from
+  id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/apis/case-api"
 }
