f417f4acf91b1e5e0b8f2ef79daf4e75684cedac
bharadwajsairam <bharadwajsairam001@gmail.com>
2024-11-11 18:17:57 +0000

    Remove duplicates

--- import.tf
+++ import.tf
@@ -7,16 +7,7 @@ import {
   # Specify the resource address to import to
   to = module.document-mgmt-api.azurerm_api_management_api.api
   # Specify the resource ID to import from
-  id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/apis/document-api"
-}
-
-import {
-  # Run this import only in prod
-  for_each = var.env == "demo" ? toset(["import"]) : toset([])
-  # Specify the resource address to import to
-  to = module.document-mgmt-api.azurerm_api_management_api.api
-  # Specify the resource ID to import from
-  id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/apis/case-search-api"
+  id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/apis"
 }
 
 import {
@@ -43,7 +34,7 @@ import {
   # Specify the resource address to import to
   to = module.case-document-mgmt-product.azurerm_api_management_product.product
   # Specify the resource ID to import from
-  id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/products/fis-document-get"
+  id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/products"
 }
 
 import {
@@ -53,15 +44,6 @@ import {
   to = module.case-document-mgmt-api.azurerm_api_management_api.api
   # Specify the resource ID to import from
   id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/apis/fis-document-get-api"
-}
-
-import {
-  # Run this import only in prod
-  for_each = var.env == "demo" ? toset(["import"]) : toset([])
-  # Specify the resource address to import to
-  to = module.case-document-mgmt-product.azurerm_api_management_product.product
-  # Specify the resource ID to import from
-  id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/products/case-search"
 }
 
 import {
