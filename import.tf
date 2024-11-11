eb472bb123fcb2201fb61148e7adcf39a25cd7d0
58060 <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>
2024-11-11 14:09:17 +0000

    Updating Terraform Formatting

--- import.tf
+++ import.tf
@@ -5,7 +5,7 @@ import {
   # Run this import only in prod
   for_each = var.env == "demo" ? toset(["import"]) : toset([])
   # Specify the resource address to import to
-  to =  azurerm_api_management_api
+  to = azurerm_api_management_api
   # Specify the resource ID to import from
   id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/apis/case-api;rev=1"
 }
