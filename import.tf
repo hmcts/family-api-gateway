0b428c556d8c33a163cf8cd5e921287f2b7ba19d
bharadwajsairam <bharadwajsairam001@gmail.com>
2024-11-11 18:07:32 +0000

    Update required imports

--- import.tf
+++ import.tf
@@ -5,9 +5,9 @@ import {
   # Run this import only in prod
   for_each = var.env == "demo" ? toset(["import"]) : toset([])
   # Specify the resource address to import to
-  to = module.document-mgmt-product.azurerm_api_management_product.product
+  to = module.document-mgmt-api.azurerm_api_management_api.api
   # Specify the resource ID to import from
-  id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/products/document"
+  id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/apis/document-api"
 }
 
 import {
@@ -16,5 +16,59 @@ import {
   # Specify the resource address to import to
   to = module.document-mgmt-api.azurerm_api_management_api.api
   # Specify the resource ID to import from
+  id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/apis/case-search-api"
+}
+
+import {
+  # Run this import only in prod
+  for_each = var.env == "demo" ? toset(["import"]) : toset([])
+  # Specify the resource address to import to
+  to = module.case-mgmt-api.azurerm_api_management_api.api
+  # Specify the resource ID to import from
   id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/apis/case-api"
+}
+
+import {
+  # Run this import only in prod
+  for_each = var.env == "demo" ? toset(["import"]) : toset([])
+  # Specify the resource address to import to
+  to = module.document-mgmt-product.azurerm_api_management_product_group.access_control_groups["developers"]
+  # Specify the resource ID to import from
+  id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/products/document/groups/developers"
+}
+
+import {
+  # Run this import only in prod
+  for_each = var.env == "demo" ? toset(["import"]) : toset([])
+  # Specify the resource address to import to
+  to = module.case-document-mgmt-product.azurerm_api_management_product.product
+  # Specify the resource ID to import from
+  id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/products/fis-document-get"
+}
+
+import {
+  # Run this import only in prod
+  for_each = var.env == "demo" ? toset(["import"]) : toset([])
+  # Specify the resource address to import to
+  to = module.case-document-mgmt-api.azurerm_api_management_api.api
+  # Specify the resource ID to import from
+  id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/apis/fis-document-get-api"
+}
+
+import {
+  # Run this import only in prod
+  for_each = var.env == "demo" ? toset(["import"]) : toset([])
+  # Specify the resource address to import to
+  to = module.case-document-mgmt-product.azurerm_api_management_product.product
+  # Specify the resource ID to import from
+  id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/products/case-search"
+}
+
+import {
+  # Run this import only in prod
+  for_each = var.env == "demo" ? toset(["import"]) : toset([])
+  # Specify the resource address to import to
+  to = module.api-case-mgmt-product.azurerm_api_management_product.product
+  # Specify the resource ID to import from
+  id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/products/case"
 }
