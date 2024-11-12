dd808b18b93c91249c1a34a1d6df8b973d53463d
bharadwajsairam <bharadwajsairam001@gmail.com>
2024-11-12 09:53:41 +0000

    Import non existent object : module.case-mgmt-api.azurerm_api_management_api.api removeed

--- import.tf
+++ import.tf
@@ -41,15 +41,6 @@ import {
   # Run this import only in prod
   for_each = var.env == "demo" ? toset(["import"]) : toset([])
   # Specify the resource address to import to
-  to = module.case-mgmt-api.azurerm_api_management_api.api
-  # Specify the resource ID to import from
-  id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/apis/case-api"
-}
-
-import {
-  # Run this import only in prod
-  for_each = var.env == "demo" ? toset(["import"]) : toset([])
-  # Specify the resource address to import to
   to = module.case-search-mgmt-api.azurerm_api_management_api.api
   # Specify the resource ID to import from
   id = "/subscriptions/d025fece-ce99-4df2-b7a9-b649d3ff2060/resourceGroups/cft-demo-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-demo/apis/case-search-api"
