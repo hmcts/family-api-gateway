370ca98ba8d7a407c651f9bf30dfb59ba0143ef8
BharadwajSairam <75499453+BharadwajSairam@users.noreply.github.com>
2024-11-25 14:47:04 +0000

    Update import.tf

--- import.tf
+++ import.tf
@@ -3,12 +3,12 @@ data "azurerm_subscription" "current" {}
 
 import {
   for_each = var.env == "perftest" ? toset(["import"]) : toset([])
-  to       = data.azurerm_key_vault_secret.case_creation_subscription_key
+  to       = azurerm_key_vault_secret.case_creation_subscription_key
   id       = "https://fis-kv-perftest.vault.azure.net/secrets/courtnav-subscription-sub-key/43b83fe942ea446c91777cfbcff2a638"
 }
 
 import {
   for_each = var.env == "perftest" ? toset(["import"]) : toset([])
-  to       = data.azurerm_key_vault_secret.document_subscription_key
+  to       = azurerm_key_vault_secret.document_subscription_key
   id       = "https://fis-kv-perftest.vault.azure.net/secrets/document-subscription-sub-key/0b08e836176646cb9a1adfed73dc50ce"
 }
